a74f0744bd513a022de456b215413cfca4558e66
João Magalhães <joamag@gmail.com>
2022-06-16 17:51:45 +0100

    feat: better benchmark support

--- examples/benchmark/src/main.rs
+++ examples/benchmark/src/main.rs
@@ -1,7 +1,8 @@
 use chip_ahoyto::chip8::Chip8;
 use std::{fs::File, io::Read};
+use time::Instant;
 
-const CYCLE_COUNT: u32 = 1_000_000_000;
+const CYCLE_COUNT: u64 = 10_000_000_000;
 
 fn main() {
     let rom = read_file("./resources/pong.ch8");
@@ -10,11 +11,21 @@ fn main() {
     chip8.reset_hard();
     chip8.load_rom(&rom);
 
-    println!("Running {} cycles", CYCLE_COUNT);
+    let instant = Instant::now();
+
+    let cycles = CYCLE_COUNT;
+
+    println!("Running {} cycles", cycles);
 
     for _ in 0..CYCLE_COUNT {
         chip8.tick();
     }
+
+    let duration_s = instant.elapsed().as_seconds_f32();
+    let cycles_second = cycles as f32 / duration_s;
+    let mega_second = cycles_second / 1000.0 / 1000.0;
+
+    println!("Took {} seconds or {:.2} MHz CPU", duration_s, mega_second);
 }
 
 fn read_file(path: &str) -> Vec<u8> {
